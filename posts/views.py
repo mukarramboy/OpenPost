3197151e9e97aa6c0872e6703b815d8632d2c4d6
Mukarramboy Umarov <mukarramboyumarov@gmail.com>
2025-08-23 22:23:41 +0500

    add post_list

--- posts/views.py
+++ posts/views.py
@@ -6,3 +6,8 @@ from .models import Post
 def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     return render(request, 'post_detail.html', {'post': post})
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'post_list.html', {'posts': posts})
